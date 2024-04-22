1e35bd99eaaddd825a5b4da70f8e5037e64f5919
PC <PC@DESKTOP-2VPR9IB>
2024-04-21 23:13:22 -0300

    update root.css, app.css and app.js with create component header and directory components

---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+import '../styles/App.css'
+
+const Header = () => {
+
+ return (
+    <>
+     <header>
+        <nav>
+            <ul>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+            </ul>
+        </nav>
+     </header>
+    </>
+ )
+}
+
+export default Header;
